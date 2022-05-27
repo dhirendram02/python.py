cb2421b2191c866081eedc46c198f585a89403a1
Bhuwanesh Tripathi <Bhuwanesh340@gmail.com>
2022-05-27 20:54:21 +0530

    print reverse of hlf diamond pattern

---
+++ print_reverse_of_half_diamond_pattern_of_n_number.py
@@ -0,0 +1,9 @@
+n = int(input("enter the rows "))
+k = 2 * n - 2
+for i in range(n, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1
+    for j in range(0, i+1):
+        print("*", end=" ")
+    print("")
