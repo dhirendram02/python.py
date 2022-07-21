e07a4714b0f5b16347ab2f985ecadb4ee5abe9ad
Bhuwanesh Tripathi <Bhuwanesh340@gmail.com>
2022-07-21 10:30:48 +0530

    check the matrix is equal or not

---
+++ check_matix_is_equal_or_not.py
@@ -0,0 +1,12 @@
+lst1=[[1,44,55],
+     [2,45,66],
+     [3,77,91]]
+
+lst2=[[1,44,55],
+     [2,45,66],
+     [3,77,91]]
+
+if (lst1 == lst2):
+    print("matrix is equal.")
+else:
+    print("matrix is not equal.")
