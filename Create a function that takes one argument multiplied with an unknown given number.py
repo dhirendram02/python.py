499c325cf87144853e847ef922445ab78562df0f
Dhirendra Kumar <74134523+dhirendram02@users.noreply.github.com>
2022-09-10 10:50:49 +0530

    Create Create a function that takes one argument multiplied with an unknown given number.py

---
+++ Create a function that takes one argument multiplied with an unknown given number.py
@@ -0,0 +1,10 @@
+def func_com(n):
+ return lambda x : x * n
+result = func_com(2)
+print(" 15 =", result(15))
+result = func_com(3)
+print(" 15 =", result(15))
+result = func_com(4)
+print(" 15 =", result(15))
+result = func_com(5)
+print(" 15 =", result(15))
